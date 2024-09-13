5d8948c511ccf6835f7338e944f67b3eabf0bea1
coderpro <96303261+Meedo2011@users.noreply.github.com>
2024-09-13 09:27:30 +0300

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Initialize EmailJS with your Public User ID
-    emailjs.init('Uw_ghGZEZ0Zq3eQLC');  // Replace with your EmailJS Public User ID
+    // Initialize EmailJS with your User ID
+    emailjs.init('Uw_ghGZEZ0Zq3eQLC');  // Your EmailJS User ID
 
     // Handle Subscribe form submission
     document.getElementById('subscribeForm').addEventListener('submit', function(event) {
         event.preventDefault();
 
         // Send email using EmailJS
-        emailjs.sendForm('service_xmu5ag5', 'template_izg9kmt', this)
+        emailjs.sendForm('service_xmu5ag5', 'template_cg7nxik', this)
             .then(() => {
-                document.querySelector('.thank-you').style.display = 'block';
+                const thankYouElement = document.querySelector('.thank-you');
+                thankYouElement.style.display = 'block';
+                setTimeout(() => {
+                    thankYouElement.style.display = 'none';
+                }, 5000);  // Hides the message after 5 seconds
                 this.reset();  // Clear the form
             }, (error) => {
                 console.error('Failed to send email. Error: ', error);
@@ -21,9 +25,13 @@ document.addEventListener('DOMContentLoaded', () => {
         event.preventDefault();
 
         // Send email using EmailJS
-        emailjs.sendForm('service_xmu5ag5', 'template_izg9kmt', this)
+        emailjs.sendForm('service_xmu5ag5', 'template_cg7nxik', this)
             .then(() => {
-                document.querySelector('.thank-you-message').style.display = 'block';
+                const thankYouMessageElement = document.querySelector('.thank-you-message');
+                thankYouMessageElement.style.display = 'block';
+                setTimeout(() => {
+                    thankYouMessageElement.style.display = 'none';
+                }, 5000);  // Hides the message after 5 seconds
                 this.reset();  // Clear the form
             }, (error) => {
                 console.error('Failed to send email. Error: ', error);
